a2781b12addf6dd22d2c257e72e4b01c83b5ff7c
Praveen Bannaiah <pbannai@grendel43.ece.ncsu.edu>
2025-12-23 15:50:50 -0500

    AXI state machine draft

--- project_benches/axi_to_apb/rtl/dut.sv
+++ project_benches/axi_to_apb/rtl/dut.sv
@@ -13,10 +13,12 @@ module AXI_to_APB_Bridge_DUT
     input logic [ADDR_WIDTH:0] awaddr,
     input logic awvalid,
     output logic awready,
+    input logic [3:0] aw_transaction_id,
     
     //W Channel
     input logic [ADDR_WIDTH:0] wdata,
     input logic wvalid,
+    input logic [3:0] w_transaction_id,
     output logic wready,
     input logic wlast,
 
@@ -24,14 +26,17 @@ module AXI_to_APB_Bridge_DUT
     input logic bready,
     output logic bvalid,
     output logic [1:0] bresp,     //00: OKAY, 01: EXOKAY, 10: SLVERR, 11: DECERR
+    output logic [3:0] b_transaction_id,
 
     //AR Channel
     input logic [DATA_WIDTH:0] araddr,
     input logic arvalid,
+    input logic [3:0] ar_transaction_id,
     output logic arready,
 
     //R Channel
     input logic rready,
+    output logic [3:0] r_transaction_id,
     output logic rvalid,
     output logic [DATA_WIDTH:0] rdata,
     output logic [1:0] rresp,     //00: OKAY, 01: EXOKAY, 10: SLVERR, 11: DECERR
@@ -78,8 +83,8 @@ module AXI_to_APB_Bridge_DUT
     //B Channel state machine
     typedef enum logic [1:0] {
         B_IDLE     = 2'b00,
-        B_VALID    = 2'b01,
-        B_READY    = 2'b10
+        B_GET_RESPONSE    = 2'b01,
+        B_RESPOND    = 2'b10
     } b_state_t;
 
     b_state_t b_current_state, b_next_state;
@@ -88,7 +93,8 @@ module AXI_to_APB_Bridge_DUT
     typedef enum logic [1:0] {
         AR_IDLE     = 2'b00,
         AR_VALID    = 2'b01,
-        AR_READY    = 2'b10
+        AR_READY    = 2'b10,
+        AR_SAMPLE   = 2'b11
     } ar_state_t;   
     
     ar_state_t ar_current_state, ar_next_state;
@@ -96,8 +102,8 @@ module AXI_to_APB_Bridge_DUT
     //R Channel state machine
     typedef enum logic [1:0] {      
         R_IDLE     = 2'b00,
-        R_VALID    = 2'b01,
-        R_READY    = 2'b10
+        R_DATA    = 2'b01,
+        R_VALID    = 2'b10  //FIXME: Add support for multiple/burst transactions later (RLAST)
        // R_LAST     = 2'b11
     } r_state_t;
 
@@ -194,6 +200,12 @@ module AXI_to_APB_Bridge_DUT
             AW_VALID: begin
                 //check if we are ready to sample the address
                 //Add FIFO check
+                if (address_fifo_full()) begin
+                    aw_next_state = AW_VALID;
+                end
+                else begin
+                    aw_next_state = AW_READY;
+                end
 
             end
             AW_READY: begin
@@ -204,7 +216,179 @@ module AXI_to_APB_Bridge_DUT
             AW_SAMPLE: begin
                 //Sample the address and store in in the buffer
                 //Add support for multiple/burst transactions later
+                addr_t transaction;
+                if(awaddr != 32'bz || awaddr != 32'bx) begin
+                    transaction.awaddr = awaddr;
+                    transaction.transaction_id = aw_transaction_id;
+                    transaction.read_write_flag = 1'b1; //Write Operation
+                    Address_trans_buffer[address_write_pointer++] = transaction;
+                    
+                    if (address_write_pointer == outstandin_transactions) begin
+                        address_write_pointer = 8'b0;
+                        address_write_phase_bit = ~address_write_phase_bit;
+                    end 
+                end
 
+                aw_next_state = AW_IDLE;
+
+            end
+
+        endcase
+
+    end
+
+
+    always_comb begin
+        //AR Channel State Machine
+        case(ar_current_state)
+            AR_IDLE: begin
+                arready = 1'b0;
+                if(arvalid)
+                    ar_next_state = AR_VALID;
+                else
+                    ar_next_state = AR_IDLE;
+            end
+
+            AR_VALID: begin
+                //check if we are ready to sample the address
+                //Add FIFO check
+                if (address_fifo_full()) begin
+                    ar_next_state = AR_VALID;
+                end
+                else begin
+                    ar_next_state = AR_READY;
+                end
+
+            end
+            AR_READY: begin
+                arready = 1'b1;
+                ar_next_state = AR_SAMPLE;
+            end
+
+            AR_SAMPLE: begin
+                //Sample the address and store in in the buffer              
+                addr_t transaction;
+                if(araddr != 32'bz || araddr != 32'bx) begin
+                    transaction.araddr = araddr;
+                    transaction.transaction_id = ar_transaction_id;
+                    transaction.read_write_flag = 1'b0; //Read Operation
+                    Address_trans_buffer[address_write_pointer++] = transaction;
+                    
+                    if (address_write_pointer == outstandin_transactions) begin
+                        address_write_pointer = 8'b0;
+                        address_write_phase_bit = ~address_write_phase_bit;
+                    end 
+                end
+
+                ar_next_state = AR_IDLE;
+
+            end
+
+        endcase
+
+    end
+
+
+    always_comb begin
+        //W Channel State Machine
+        case(w_current_state)
+            W_IDLE: begin
+                //Check if we are ready to sample the data
+                wready = 1'b0;
+                if(axi_fifo_full()) begin
+                    w_next_state = W_IDLE;
+                end
+                else begin
+                    w_next_state = W_VALID;
+                end
+
+            end
+
+            W_READY: begin
+                wready = 1'b1;
+                if(axi_fifo_full()) begin
+                    w_next_state = W_IDLE;
+                end
+                else if(wvalid) begin
+                    w_next_state = W_VALID;
+                end
+                else 
+                    w_next_state = W_READY;
+            end
+
+            W_VALID:
+            begin
+                //Sample the data and store in the buffer
+                axi_tran_t transaction;
+                transaction.wdata = wdata;
+                transaction.read_write_flag = 1'b1; //Write Operation
+                transaction.transaction_id = w_transaction_id;
+
+                axi_trans_buffer[axi_write_pointer++] = transaction;
+                
+                if (axi_write_pointer == outstandin_transactions) begin
+                    axi_write_pointer = 8'b0;
+                    axi_write_phase_bit = ~axi_write_phase_bit;
+                end 
+
+
+                if(wlast)
+                    w_next_state = W_IDLE;
+                else
+                    w_next_state = W_READY;
+
+            end
+        endcase
+        
+
+    end
+
+
+
+    always_comb begin
+
+        //R Channel State Machine
+        case(r_current_state)
+            R_IDLE: begin
+                rvalid = 1'b0;
+                if(rready)
+                    r_next_state = R_DATA;
+                else
+                    r_next_state = R_IDLE;
+            end
+            R_DATA: begin
+                //Get response
+                rvalid = 1'b1;
+                rdata = prdata; //FIXME: Get actual response from APB
+                rresp = 2'b00; //OKAY    //FIXME: Get actual response from APB
+                
+                r_next_state = R_IDLE;
+                
+            end
+
+        endcase
+
+    end
+
+    always_comb begin
+
+        //B Channel State Machine
+        case(b_current_state)
+            B_IDLE: begin
+                bvalid = 1'b0;
+                if(bready)
+                    b_next_state = B_GET_RESPONSE;
+                else
+                    b_next_state = B_IDLE;
+            end
+            B_GET_RESPONSE: begin
+                //Get response
+                bvalid = 1'b1;
+                 
+                bresp = 2'b00; //OKAY    //FIX_ME: Get actual response from APB
+                
+                b_next_state = B_IDLE;
+                
             end
 
         endcase
